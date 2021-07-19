e94170704660389dade87ccd67966bc4dde732ff
thana19 <thana.j@gmail.com>
2021-07-19 17:26:49 +0700

    add bcrypt

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # node-bcrypt
+
+# npm init -y 
+
+# npm i -S fastify 
+
+# npm i -S bcrypt  
+
+curl http://localhost:4000 
+OK
+
+curl http://localhost:4000/bcrypt/thana
+$2b$05$jbQw1.o07kzyZi1/Jh/1pOouHMmtSAYtnvhV.EaQtOGhV5RdtukKa% 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Fastify = require('fastify')
+const bcrypt = require('bcrypt')
 
 const hostname = '0.0.0.0'
 const port = 4000
@@ -10,4 +11,16 @@ app.listen(port, hostname, () => {
 
 app.get('/', (request, reply) => {
     reply.send('OK')
+})
+
+app.get('/bcrypt/:password', async (request, reply) => {
+    const { password } = request.params
+    console.log('password ->', password)
+
+    const setRounds = 5
+    const salt = await bcrypt.genSalt(setRounds)  
+    const passwordHashed = await bcrypt.hash(password, salt)
+    console.log('passwordHashed ->', passwordHashed)
+
+    reply.send(passwordHashed)
 })
